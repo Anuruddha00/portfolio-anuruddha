d600045083e72acae859acbcbeec2541556befdb
Anuruddha00 <anuruddhabandara06@gmail.com>
2021-05-04 12:48:19 -0800

    finalize

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,18 +4,25 @@ import axios from '../axios-contact';
 
 class Contact extends Component{
 
+    state={
+        name:'',
+        subject:'',
+        email:'',
+        message:''   
+    }
 
-    contactDataHandler=(event)=>{
+    contactDataHandler=()=>{
         const contact={
-            Name:'ssa',
-            Subject:event.target.value,
-            Email:event.target.value,
-            Massage:event.target.value
+            name:this.state.name,
+            subject:this.state.subject,
+            email:this.state.email,
+            message:this.state.message
 
         }
-        axios.post('./contact.json',contact)
-        .then(response=>console.log(response))
-        .catch(error=>console.log(error))
+        axios.post('/contact.json',contact)
+        .then(response=>{
+            console.log(response)
+        })
     }
 
     render(){
@@ -23,10 +30,21 @@ class Contact extends Component{
             <div className='contact'>
                 <form className='contactForm'>
                     <h1 className='contactFormTitle'>Contact</h1>
-                    Name: <input type='text' placeholder='Name' />
-                    Subject: <input type='text' placeholder='Subject'/>
-                    Email: <input type='text' placeholder='Email'/>
-                    Message: <input className='contactFormMessage' type='text' placeholder='Message'/>
+                    Name: <input type='text' placeholder='Name' 
+                            value={this.state.name} 
+                            onChange={(event)=>this.setState({name:event.target.value})} />
+
+                    Subject: <input type='text' placeholder='Subject'
+                                value={this.state.subject} 
+                                onChange={(event)=>this.setState({subject:event.target.value})}/>
+
+                    Email: <input type='text' placeholder='Email'
+                                value={this.state.email} 
+                                onChange={(event)=>this.setState({email:event.target.value})}/>
+
+                    Message: <input className='contactFormMessage' type='text' placeholder='Message'
+                                value={this.state.message} 
+                                onChange={(event)=>this.setState({message:event.target.value})}/>
                     <button className='contactFormSend' onClick={this.contactDataHandler}>Send</button>
                 </form>
             </div>
